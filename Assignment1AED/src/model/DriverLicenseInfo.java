30ee6a501e8711f9721fb83924cc4e5ead9bc28b
Jayesh Nagarkar <nagarkar.j@northeastern.edu>
2021-02-08 12:52:42 -0500

    programming part is done,ui alignment and font color size left

---
+++ Assignment1AED/src/model/DriverLicenseInfo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author acer
+ */
+public class DriverLicenseInfo {
+    private String licenseNumber;
+    private String dateIssue;
+    private String dateExpiry;
+    private String bloodType;
+    private String imagePath;
+    
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getDateIssue() {
+        return dateIssue;
+    }
+
+    public void setDateIssue(String dateIssue) {
+        this.dateIssue = dateIssue;
+    }
+
+    public String getDateExpiry() {
+        return dateExpiry;
+    }
+
+    public void setDateExpiry(String dateExpiry) {
+        this.dateExpiry = dateExpiry;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+    
+  
+    
+}
